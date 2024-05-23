9fd7265bb12b50029a6e22795a4de3e34e1ac4ed
kajj8808 <a01039388808@gmail.com>
2024-05-23 17:41:35 +0900

    📝 dynamic + mdx

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,10 +1,36 @@
 import React from "react";
 import Layout from "../../components/layout";
+import { graphql } from "gatsby";
+import Seo from "../../components/seo";
 
-export default function BlogPost() {
+interface BlogPostProps {
+  data: Queries.PostDetailQuery;
+  children: any;
+}
+
+export default function BlogPost({ data, children }: BlogPostProps) {
   return (
-    <Layout title="Blog Post">
-      <div></div>
+    <Layout title="">
+      <div>{children}</div>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query PostDetail($frontmatter__slug: String) {
+    mdx(frontmatter: { slug: { eq: $frontmatter__slug } }) {
+      frontmatter {
+        title
+        category
+        date(formatString: "YYYY.MM.DD")
+        author
+        slug
+      }
+      body
+    }
+  }
+`;
+
+export const Head = ({ data }: BlogPostProps) => (
+  <Seo title={data.mdx?.frontmatter?.title!} />
+);
